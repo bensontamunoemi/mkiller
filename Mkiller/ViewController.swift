d5a6fa892489186728808a0b5d7dfe169879cd3c
Benson Tamunoemi <benson.codevigor@gmail.com>
2023-08-13 18:54:50 -0400

    Initial Commit

---
+++ Mkiller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Mkiller
+//
+//  Created by Tamunoemi  Benson  on 13/08/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
